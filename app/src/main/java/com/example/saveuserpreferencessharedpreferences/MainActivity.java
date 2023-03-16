f32537f10399d4415e7401bea43101990d806919
Magdi <86538068+MMIsmail89@users.noreply.github.com>
2023-03-16 17:16:29 +0200

    Shared Preferences file (write/read) editor of Shared Preferences get string put string Binding -------------- SharedPreferences prefs = getSharedPreferences("myPrefs", Context.MODE_PRIVATE); SharedPreferences.Editor editor = prefs.edit(); editor.putString("name", "John"); editor.putInt("age", 25); editor.putBoolean("isMarried", false); editor.commit(); ----------------------------- SharedPreferences prefs = getSharedPreferences("myPrefs", Context.MODE_PRIVATE); String name = prefs.getString("name", "Unknown"); int age = prefs.getInt("age", 0); boolean isMarried = prefs.getBoolean("isMarried", false);

---
+++ app/src/main/java/com/example/saveuserpreferencessharedpreferences/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.saveuserpreferencessharedpreferences;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.saveuserpreferencessharedpreferences.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    //
+    public static final String Prefs_FileName = "usersNameFile";
+    public static final String Key_UserName = "userName";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        //
+        SharedPreferences preferences = getSharedPreferences(Prefs_FileName, MODE_PRIVATE);
+        String userName = preferences.getString(Key_UserName, null);
+        //
+        if (userName!=null) {
+            binding.mainTvTitle.setText(R.string.welcomeAppDear + userName);}
+        //
+        binding.mainBtnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = binding.mainEtDispName.getText().toString().trim();
+                if (name.isEmpty()){
+                    Toast.makeText(MainActivity.this,
+                            getString(R.string.valid_name_required), Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    binding.mainTvTitle.setText(R.string.welcomeApp + name + "!");
+                    //
+                    SharedPreferences.Editor editor = getSharedPreferences(Prefs_FileName, MODE_PRIVATE).edit();
+                    editor.putString(Key_UserName, name);
+                    editor.commit();
+                    //
+                    Intent intent = new Intent(MainActivity.this, UserActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/saveuserpreferencessharedpreferences/UserActivity.java
@@ -0,0 +1,28 @@
+package com.example.saveuserpreferencessharedpreferences;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.saveuserpreferencessharedpreferences.databinding.ActivityMainBinding;
+import com.example.saveuserpreferencessharedpreferences.databinding.ActivityUserBinding;
+
+public class UserActivity extends AppCompatActivity {
+
+    ActivityUserBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //
+        binding = ActivityUserBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        //
+        SharedPreferences preferences = getSharedPreferences(MainActivity.Prefs_FileName, MODE_PRIVATE);
+        String userName = preferences.getString(MainActivity.Key_UserName, null);
+        //
+        binding.userActivityTvDispName.setText(R.string.welcomeAppDear + userName);
+    }
+}
